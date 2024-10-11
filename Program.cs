d29332504d5627e37fbb51bdbf6e55ecf5b52485
BenceR27 <rizb@kkszki.hu>
2024-10-11 11:21:56 +0200

    the first commit is ready

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace napszak
+{
+    internal class Program
+    {
+        public static void udvozol();
+
+        string reggel = "Jó reggelt!";
+        string delutan = "Szép délutánt!";
+        string este = "Jó estét!";
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
